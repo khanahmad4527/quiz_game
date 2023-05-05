d22bd970d06a1dcbff34c338c4917469c4910cdd
khanahmad4527 <khanahmad4527@gmail.com>
2023-05-05 11:47:27 +0530

    added more categories

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -80,25 +80,52 @@ const Home = () => {
             <FormControl isRequired>
               <FormLabel>Category</FormLabel>
               <Select
-                placeholder="Select category"
                 name="category"
                 value={form.category}
                 onChange={handleFormChange}
               >
+                <option value="any">Any Category</option>
                 <option value="9">General Knowledge</option>
+                <option value="10">Entertainment: Books</option>
+                <option value="11">Entertainment: Film</option>
+                <option value="12">Entertainment: Music</option>
+                <option value="13">
+                  Entertainment: Musicals &amp; Theatres
+                </option>
+                <option value="14">Entertainment: Television</option>
+                <option value="15">Entertainment: Video Games</option>
+                <option value="16">Entertainment: Board Games</option>
+                <option value="17">Science &amp; Nature</option>
+                <option value="18">Science: Computers</option>
+                <option value="19">Science: Mathematics</option>
+                <option value="20">Mythology</option>
                 <option value="21">Sports</option>
                 <option value="22">Geography</option>
+                <option value="23">History</option>
+                <option value="24">Politics</option>
+                <option value="25">Art</option>
+                <option value="26">Celebrities</option>
+                <option value="27">Animals</option>
+                <option value="28">Vehicles</option>
+                <option value="29">Entertainment: Comics</option>
+                <option value="30">Science: Gadgets</option>
+                <option value="31">
+                  Entertainment: Japanese Anime &amp; Manga
+                </option>
+                <option value="32">
+                  Entertainment: Cartoon &amp; Animations
+                </option>
               </Select>
             </FormControl>
 
             <FormControl isRequired>
               <FormLabel>Difficulty level</FormLabel>
               <Select
-                placeholder="Select level"
                 name="level"
                 value={form.level}
                 onChange={handleFormChange}
               >
+                <option value="any">Any Difficulty</option>
                 <option value="easy">Easy</option>
                 <option value="medium">Medium</option>
                 <option value="hard">Hard</option>
